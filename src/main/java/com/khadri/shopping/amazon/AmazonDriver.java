c36236f037a16bc1b2f167de224e00652f9fbd1a
sgreddy123 <151907854+sgreddy123@users.noreply.github.com>
2024-07-25 23:15:34 +0530

    resolved conflicts

--- src/main/java/com/khadri/shopping/amazon/AmazonDriver.java
+++ src/main/java/com/khadri/shopping/amazon/AmazonDriver.java
@@ -17,12 +17,12 @@ public class AmazonDriver {
 		do {
 
 			System.out.println("########## Home Page ##########");
-			System.out.println("1.Mens_wear");
-			System.out.println("2.Kids_wear");
-			System.out.println("3.Womens_wear");
-			System.out.println("4.Previous");
+			System.out.println(" 1.Mens_wear ");
+			System.out.println(" 2.Kids_wear ");
+			System.out.println(" 3.Womens_wear ");
+			System.out.println(" 4.Previous ");
 
-			System.out.println("Choose any one Category:(1,2,3) ");
+			System.out.println("#####Choose any one Category:(1,2,3)##### ");
 			System.out.println("=============================");
 			int option = scanner.nextInt();
 			switch (option) {
@@ -37,11 +37,11 @@ public class AmazonDriver {
 				break;
 
 			case 4:
-				System.out.println("Thank you for shopping!");
+				System.out.println("Thank you for shopping!!!!!!!!!");
 				scanner.close();
 				return;
 			default:
-				System.out.println("Invalid choice. Please try again.");
+				System.out.println("######Invalid choice. Please try again.######");
 				System.exit(0);
 			}
 
